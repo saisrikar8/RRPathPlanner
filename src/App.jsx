d39093f89a85ff8fd2521dfcb2f81abee32d8377
saisrikar8 <77305508+saisrikar8@users.noreply.github.com>
2026-04-13 22:42:52 -0700

    fixing analytics

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import {MotionEngine, buildDriveTimeProfile} from "./components/MotionEngine.js"
 import MotionParamsModal from "./components/MotionParamsModal/MotionParamsModal.jsx";
 import ExportModal from "./components/ExportModal/ExportModal.jsx";
 import ImportModal from "./components/ImportModal/ImportModal.jsx";
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from "@vercel/analytics/react"
 
 const DEFAULT_MOTION = {
     maxVel: 50,
